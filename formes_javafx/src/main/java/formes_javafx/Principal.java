4021329b958f2787779dd1cf12d0e795d0041b99
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-01-23 14:36:35 -0500

    [Coquille]

--- formes_javafx/src/main/java/formes_javafx/Principal.java
+++ formes_javafx/src/main/java/formes_javafx/Principal.java
@@ -13,114 +13,114 @@ import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 
 public class Principal extends Application {
-	
-	private static final String CHEMIN_FICHIER_ENTREE = "/entrees/01.txt";
+    
+    private static final String CHEMIN_FICHIER_ENTREE = "/entrees/01.txt";
 
-	private static final int HAUTEUR = 400;
-	private static final int LARGEUR = 600;
+    private static final int HAUTEUR = 400;
+    private static final int LARGEUR = 600;
 
-	private GraphicsContext dessin;
+    private GraphicsContext dessin;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		launch(args);
-	}
-	
-	@Override
-	public void start(Stage stage) throws Exception {
-		
-		Scene scene = creerScene();
-		
-		lireFichierEtAfficherFormes();
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Scene scene = creerScene();
+        
+        lireFichierEtAfficherFormes();
 
-		stage.setScene(scene);
-		stage.show();
-	}
+        stage.setScene(scene);
+        stage.show();
+    }
 
-	private void lireFichierEtAfficherFormes() {
+    private void lireFichierEtAfficherFormes() {
 
-		InputStream streamEntree = Principal.class.getResourceAsStream(CHEMIN_FICHIER_ENTREE);
+        InputStream streamEntree = Principal.class.getResourceAsStream(CHEMIN_FICHIER_ENTREE);
 
-		lireFichierEtAfficherFormes(streamEntree);
-	}
+        lireFichierEtAfficherFormes(streamEntree);
+    }
 
-	private Scene creerScene() {
+    private Scene creerScene() {
 
-		Canvas canvas = new Canvas(LARGEUR, HAUTEUR);
-		dessin = canvas.getGraphicsContext2D();
-		
-		Pane pane = new Pane();
-		pane.getChildren().add(canvas);
+        Canvas canvas = new Canvas(LARGEUR, HAUTEUR);
+        dessin = canvas.getGraphicsContext2D();
+        
+        Pane pane = new Pane();
+        pane.getChildren().add(canvas);
 
-		Scene scene = new Scene(pane, LARGEUR, HAUTEUR);
+        Scene scene = new Scene(pane, LARGEUR, HAUTEUR);
 
-		return scene;
-	}
+        return scene;
+    }
 
-	private void lireFichierEtAfficherFormes(InputStream streamEntree) {
+    private void lireFichierEtAfficherFormes(InputStream streamEntree) {
 
-		Scanner scan = new Scanner(streamEntree);
-		
-		while(scan.hasNext()) {
-			lireEtAfficherUneForme(scan);
-		}
+        Scanner scan = new Scanner(streamEntree);
+        
+        while(scan.hasNext()) {
+            lireEtAfficherUneForme(scan);
+        }
 
-		scan.close();
-	}
+        scan.close();
+    }
 
-	private void lireEtAfficherUneForme(Scanner scan) {
+    private void lireEtAfficherUneForme(Scanner scan) {
 
-		String typeForme = scan.next();
+        String typeForme = scan.next();
 
-		Color couleur = Color.valueOf(scan.next());
-		dessin.setFill(couleur);
-		
-		int centreX = scan.nextInt();
-		int centreY = scan.nextInt();
+        Color couleur = Color.valueOf(scan.next());
+        dessin.setFill(couleur);
+        
+        int centreX = scan.nextInt();
+        int centreY = scan.nextInt();
 
-		lireFichierEtAfficherUneForme(scan, typeForme, centreX, centreY);
-	}
+        lireFichierEtAfficherUneForme(scan, typeForme, centreX, centreY);
+    }
 
-	private void lireFichierEtAfficherUneForme(Scanner scan, 
-			String typeForme, 
-			int centreX,
-			int centreY) {
+    private void lireFichierEtAfficherUneForme(Scanner scan, 
+            String typeForme, 
+            int centreX,
+            int centreY) {
 
-		switch(typeForme) {
+        switch(typeForme) {
 
-			case "Cercle":
-				lireEtAfficherUnCercle(scan, centreX, centreY);
-				break;
+            case "Cercle":
+                lireEtAfficherUnCercle(scan, centreX, centreY);
+                break;
 
-			case "Carre":
-				lireEtAfficherUnCarre(scan, centreX, centreY);
-				break;
+            case "Carre":
+                lireEtAfficherUnCarre(scan, centreX, centreY);
+                break;
 
-			case "Rectangle":
-				lireEtAfficherUnRectangle(scan, centreX, centreY);
-				break;
-		}
-	}
+            case "Rectangle":
+                lireEtAfficherUnRectangle(scan, centreX, centreY);
+                break;
+        }
+    }
 
-	private void lireEtAfficherUnRectangle(Scanner scan, int centreX, int centreY) {
+    private void lireEtAfficherUnRectangle(Scanner scan, int centreX, int centreY) {
 
-		int largeur = scan.nextInt();
-		int hauteur = scan.nextInt();
+        int largeur = scan.nextInt();
+        int hauteur = scan.nextInt();
 
-		dessin.fillRect(centreX-largeur/2, centreY-hauteur/2, largeur, hauteur);
-	}
+        dessin.fillRect(centreX-largeur/2, centreY-hauteur/2, largeur, hauteur);
+    }
 
-	private void lireEtAfficherUnCarre(Scanner scan, int centreX, int centreY) {
+    private void lireEtAfficherUnCarre(Scanner scan, int centreX, int centreY) {
 
-		int taille = scan.nextInt();
+        int taille = scan.nextInt();
 
-		dessin.fillRect(centreX-taille/2, centreY-taille/2, taille, taille);
-	}
+        dessin.fillRect(centreX-taille/2, centreY-taille/2, taille, taille);
+    }
 
-	private void lireEtAfficherUnCercle(Scanner scan, int centreX, int centreY) {
+    private void lireEtAfficherUnCercle(Scanner scan, int centreX, int centreY) {
 
-		int r = scan.nextInt();
+        int rayon = scan.nextInt();
 
-		dessin.fillArc(centreX-r, centreY-r, r, r, 0, 360, ArcType.ROUND);
-	}
+        dessin.fillArc(centreX-rayon, centreY-rayon, rayon, rayon, 0, 360, ArcType.ROUND);
+    }
 }
