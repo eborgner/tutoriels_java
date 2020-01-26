6a752795aa537f2e51a163f75d99d9405568ee1e
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-01-25 21:16:54 -0500

    [En cours] atelier05; reconception ModeleLectureSeule CommandeLectureSeule

--- quatredesuite_javafx/src/main/java/quatredesuite_javafx/vues/VuePrincipaleFX.java
+++ quatredesuite_javafx/src/main/java/quatredesuite_javafx/vues/VuePrincipaleFX.java
@@ -1,9 +1,8 @@
 package quatredesuite_javafx.vues;
 
 
-
+import commun_client.Vue;
 import javafx.fxml.FXML;
-
 import javafx.scene.control.MenuItem;
 
 public class VuePrincipaleFX implements Vue {
