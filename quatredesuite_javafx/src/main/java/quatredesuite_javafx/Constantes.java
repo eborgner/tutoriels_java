02526b043f2256f06eb9f7226d3a7e69b0d534d4
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-01-27 12:44:51 -0500

    Ajustement) 20px pour la décoration de fenêtre

--- quatredesuite_javafx/src/main/java/quatredesuite_javafx/Constantes.java
+++ quatredesuite_javafx/src/main/java/quatredesuite_javafx/Constantes.java
@@ -2,18 +2,17 @@ package quatredesuite_javafx;
 
 public class Constantes {
 
-	public static final String CHEMIN_CHAINES = "traductions.chaines";
+    public static final String CHEMIN_CHAINES = "traductions.chaines";
 
-	public static final String CHEMIN_PARTIE_LOCALE_FXML = "/fxml/partieLocale.xml";
-	public static final String CHEMIN_PARTIE_LOCALE_CSS = "/css/partieLocale.css";
-	
-	public static final int LARGEUR_SCENE = 220;
-	public static final int HAUTEUR_SCENE = 380;
+    public static final String CHEMIN_PARTIE_LOCALE_FXML = "/fxml/partieLocale.xml";
+    public static final String CHEMIN_PARTIE_LOCALE_CSS = "/css/partieLocale.css";
+    
+    public static final int LARGEUR_SCENE = 220;
+    public static final int HAUTEUR_SCENE = 380;
 
-	public static final int HAUTEUR_DECORATION_FENETRE = 10;
-	
+    public static final int HAUTEUR_DECORATION_FENETRE = 20;
 
-	public static final int LARGEUR_FENETRE = LARGEUR_SCENE;
-	public static final int HAUTEUR_FENETRE = HAUTEUR_SCENE + HAUTEUR_DECORATION_FENETRE;
+    public static final int LARGEUR_FENETRE = LARGEUR_SCENE;
+    public static final int HAUTEUR_FENETRE = HAUTEUR_SCENE + HAUTEUR_DECORATION_FENETRE;
 
 }
