0e9ed3777d0b17bfa86ba78131f02984c0bab1fd
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-01-28 07:11:22 -0500

    en cours) exemple HBox

--- quatredesuite_javafx/src/main/java/quatredesuite_javafx/Constantes.java
+++ quatredesuite_javafx/src/main/java/quatredesuite_javafx/Constantes.java
@@ -7,7 +7,7 @@ public class Constantes {
     public static final String CHEMIN_PARTIE_LOCALE_FXML = "/fxml/partieLocale.xml";
     public static final String CHEMIN_PARTIE_LOCALE_CSS = "/css/partieLocale.css";
     
-    public static final int LARGEUR_SCENE = 380;
+    public static final int LARGEUR_SCENE = 420;
     public static final int HAUTEUR_SCENE = 220;
 
     public static final int HAUTEUR_DECORATION_FENETRE = 20;
