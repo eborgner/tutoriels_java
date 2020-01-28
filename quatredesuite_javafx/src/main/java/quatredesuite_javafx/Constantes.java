a3a20ba14fc53eb0794d8011d8a10eab8678645a
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-01-28 08:59:23 -0500

    en cours) exemples pour théorie

--- quatredesuite_javafx/src/main/java/quatredesuite_javafx/Constantes.java
+++ quatredesuite_javafx/src/main/java/quatredesuite_javafx/Constantes.java
@@ -7,18 +7,20 @@ public class Constantes {
     public static final String CHEMIN_PARTIE_LOCALE_FXML = "/fxml/partieLocale.xml";
     public static final String CHEMIN_PARTIE_LOCALE_CSS = "/css/partieLocale.css";
     
-    public static final int LARGEUR_SCENE = 500;
-    public static final int HAUTEUR_SCENE = 450;
+    public static final int LARGEUR_SCENE_PETIT = 400;
+    public static final int HAUTEUR_SCENE_PETIT = 300;
 
-    public static final int LARGEUR_SCENE_MAX = 800;
-    public static final int HAUTEUR_SCENE_MAX = 600;
+    public static final int LARGEUR_SCENE_GRAND = 800;
+    public static final int HAUTEUR_SCENE_GRAND = 600;
+    
+    
+    public static final int LARGEUR_SCENE= LARGEUR_SCENE_PETIT;
+    public static final int HAUTEUR_SCENE = HAUTEUR_SCENE_PETIT;
+
 
     public static final int HAUTEUR_DECORATION_FENETRE = 20;
 
     public static final int LARGEUR_FENETRE = LARGEUR_SCENE;
     public static final int HAUTEUR_FENETRE = HAUTEUR_SCENE + HAUTEUR_DECORATION_FENETRE;
-
-    public static final int LARGEUR_FENETRE_MAX = LARGEUR_SCENE_MAX;
-    public static final int HAUTEUR_FENETRE_MAX = HAUTEUR_SCENE_MAX + HAUTEUR_DECORATION_FENETRE;
 
 }
