362f6519778069aaaa107a6f1a6b0d1bda0534c1
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-01-30 12:29:52 -0500

    point de départ... encore!

--- quatredesuite_javafx/src/main/java/quatredesuite_javafx/Principal.java
+++ quatredesuite_javafx/src/main/java/quatredesuite_javafx/Principal.java
@@ -10,73 +10,70 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Principal extends Application {
-	
-	static {
-		J.appel(Principal.class);
+    
+    static {
+        J.appel(Principal.class);
 
-		Initialisateur.initialiser();
+        Initialisateur.initialiser();
 
-	}
-	
-	public static void main(String[] args) {
-		J.appel(Principal.class);
+    }
+    
+    public static void main(String[] args) {
+        J.appel(Principal.class);
 
-		launch(args);
-	}
-	
-	@Override
-	public void start(Stage fenetrePrincipale) throws Exception {
-		J.appel(this);
-		
-		Scene scene = creerScenePrincipale();
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage fenetrePrincipale) throws Exception {
+        J.appel(this);
+        
+        Scene scene = creerScenePrincipale();
 
-		afficherFenetre(fenetrePrincipale, scene);
-	}
+        afficherFenetre(fenetrePrincipale, scene);
+    }
 
-	private void afficherFenetre(Stage fenetrePrincipale, Scene scene) {
-		J.appel(this);
+    private void afficherFenetre(Stage fenetrePrincipale, Scene scene) {
+        J.appel(this);
 
-		setLargeurFenetre(fenetrePrincipale);
-		setHauteurFenetre(fenetrePrincipale);
+        setLargeurFenetre(fenetrePrincipale);
+        setHauteurFenetre(fenetrePrincipale);
 
-		fenetrePrincipale.setScene(scene);
-		fenetrePrincipale.show();
-	}
+        fenetrePrincipale.setScene(scene);
+        fenetrePrincipale.show();
+    }
 
-	private void setHauteurFenetre(Stage fenetrePrincipale) {
-		J.appel(this);
+    private void setHauteurFenetre(Stage fenetrePrincipale) {
+        J.appel(this);
 
-		fenetrePrincipale.setHeight(HAUTEUR_FENETRE);
-		fenetrePrincipale.setMinHeight(HAUTEUR_FENETRE);
-		fenetrePrincipale.setMaxHeight(HAUTEUR_FENETRE);
-	}
+        fenetrePrincipale.setHeight(HAUTEUR_FENETRE);
+        fenetrePrincipale.setMinHeight(HAUTEUR_FENETRE_MIN);
+    }
 
-	private void setLargeurFenetre(Stage fenetrePrincipale) {
-		J.appel(this);
+    private void setLargeurFenetre(Stage fenetrePrincipale) {
+        J.appel(this);
 
-		fenetrePrincipale.setWidth(LARGEUR_FENETRE);
-		fenetrePrincipale.setMinWidth(LARGEUR_FENETRE);
-		fenetrePrincipale.setMaxWidth(LARGEUR_FENETRE);
-	}
+        fenetrePrincipale.setWidth(LARGEUR_FENETRE);
+        fenetrePrincipale.setMinWidth(LARGEUR_FENETRE_MIN);
+    }
 
-	private Scene creerScenePrincipale() {
-		J.appel(this);
+    private Scene creerScenePrincipale() {
+        J.appel(this);
 
         ChargeurDeVue chargeur = new ChargeurDeVue(CHEMIN_PARTIE_LOCALE_FXML,
-        								           CHEMIN_CHAINES,
-        								           CHEMIN_PARTIE_LOCALE_CSS);
+                                                   CHEMIN_CHAINES,
+                                                   CHEMIN_PARTIE_LOCALE_CSS);
 
+        Scene scene = chargeur.nouvelleScene(LARGEUR_SCENE, HAUTEUR_SCENE);
 
-		Scene scene = chargeur.nouvelleScene(LARGEUR_SCENE, HAUTEUR_SCENE);
+        DoitEtre.nonNul(scene);
 
-		DoitEtre.nonNul(scene);
-
-		return scene;
-	}
-	
-	@Override
-	public void stop() {
-		J.appel(this);
-	}
+        return scene;
+    }
+    
+    @Override
+    public void stop() {
+        J.appel(this);
+    }
 }
 
