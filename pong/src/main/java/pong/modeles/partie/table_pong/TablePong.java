92336bd5d7622f4e4a92ef7b3345fd42561d4e88
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-02-01 15:04:36 -0500

    en cours) pong en MVC + Monde2D

--- pong/src/main/java/pong/modeles/partie/table_pong/TablePong.java
+++ pong/src/main/java/pong/modeles/partie/table_pong/TablePong.java
@@ -1,13 +1,11 @@
 package pong.modeles.partie.table_pong;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import commun.debogage.J;
 import pong.enumerations.Cadran;
 import pong.modeles.partie.monde2d.Monde2D;
-import pong.modeles.partie.monde2d.Objet2DLectureSeule;
 
 public class TablePong extends Monde2D {
 	
@@ -15,10 +13,25 @@ public class TablePong extends Monde2D {
 	
 	public TablePong(){
 		J.appel(this);
+		
+		creerPalettes();
+		ajouterPalettesCommeObjets2D();
+		
+	}
+
+	private void ajouterPalettesCommeObjets2D() {
+		J.appel(this);
+		
+		for(Palette palette : palettes.values()) {
+			objets2D.add(palette);
+		}
+	}
+
+	private void creerPalettes() {
+		J.appel(this);
 
 		palettes.put(Cadran.GAUCHE, new Palette(Cadran.GAUCHE, 0));
 		palettes.put(Cadran.DROIT, new Palette(Cadran.DROIT, 100));
-		
 	}
 
 }
