af2e5afc1db9c9fc467308e78ec7cd273d7c981f
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-02-02 08:00:59 -0500

    fait) code propre

--- quatredesuite_javafx/src/main/java/quatredesuite_javafx/vues/composants/CaseAjustable.java
+++ quatredesuite_javafx/src/main/java/quatredesuite_javafx/vues/composants/CaseAjustable.java
@@ -31,53 +31,88 @@ public class CaseAjustable extends CanvasAjustable {
 
 			private long avant = System.nanoTime();
 			private double tempsCycleSecondes = 0.5;
-			private int rougeCourant = 0;
-			private int directionIncrementRouge = 1;
+			private double facteurCourant = 0;
+			private double facteurMin = 0;
+			private double facteurMax = 1;
+			private double directionIncrement = 1;
 
 			@Override
 			public void handle(long maintenant) {
-				J.setActif(false);
+				//J.setActif(false);
 				J.appel(this);
 				
 				double secondesEcoulees = (maintenant - avant) / 1E9;
-				
-				double ratioEcoule = secondesEcoulees / tempsCycleSecondes;
-				
-				int incrementRouge = (int) (255.0 * ratioEcoule);
-
-				int prochainRouge = rougeCourant + directionIncrementRouge * incrementRouge;
-
-				if(prochainRouge > 255) {
-					prochainRouge = 255;
-					directionIncrementRouge =  -1;
-				}else if(prochainRouge < 0) {
-					prochainRouge = 0;
-					directionIncrementRouge = 1;
-				}
-				
-				int angleDepartDegre = prochainRouge * 360 / 255;
-				int incrementCouleur = prochainRouge * 35 / 255;
-				
-				viderDessin();
-				dessinerCase(TAILLE_PAR_DEFAUT_POURCENTAGE);
-
-				pinceau.setStroke(Color.rgb(220 + incrementCouleur, 220 + incrementCouleur, prochainRouge));
-				pinceau.setLineWidth(0.02*getWidth());
-				dessinerAnimation(0.8*TAILLE_PAR_DEFAUT_POURCENTAGE, angleDepartDegre);
-
-				pinceau.setStroke(Color.rgb(220 + incrementCouleur, prochainRouge, 220 + incrementCouleur));
-				pinceau.setLineWidth(0.015*getWidth());
-				dessinerAnimation(0.6*TAILLE_PAR_DEFAUT_POURCENTAGE, (angleDepartDegre+90)%360);
-
-				pinceau.setStroke(Color.rgb(prochainRouge, 220 + incrementCouleur, 220 + incrementCouleur));
-				pinceau.setLineWidth(0.010*getWidth());
-				dessinerAnimation(0.4*TAILLE_PAR_DEFAUT_POURCENTAGE, (angleDepartDegre+180)%360);
-
-				rougeCourant = prochainRouge;
 				avant = maintenant;
+
+				calculerFacteurCourant(secondesEcoulees);
+				
+				dessinerProchain();
 
 				J.setActif(true);
 			}
+
+			private void calculerFacteurCourant(double secondesEcoulees) {
+				J.appel(this);
+
+				double ratioEcoule = secondesEcoulees / tempsCycleSecondes;
+				
+				double increment = ratioEcoule * (facteurMax - facteurMin);
+
+				facteurCourant += directionIncrement * increment;
+				
+				if(facteurCourant > facteurMax) {
+
+					facteurCourant = facteurMax;
+					directionIncrement =  -1;
+
+				}else if(facteurCourant < facteurMin) {
+
+					facteurCourant = facteurMin;
+					directionIncrement = 1;
+				}
+			}
+
+			private void dessinerProchain() {
+				J.appel(this);
+
+				int prochainRGB = (int) (facteurCourant * 255);
+				int incrementCouleur = (int) (facteurCourant * 35);
+				int angleDepartDegre = (int) (facteurCourant * 360);
+				
+				viderDessin();
+				initialiserPinceau();
+				dessinerCase(TAILLE_PAR_DEFAUT_POURCENTAGE);
+				
+				dessinerPremierArc(prochainRGB, incrementCouleur, angleDepartDegre);
+				dessinerDeuxiemeArc(prochainRGB, incrementCouleur, angleDepartDegre);
+				dessinerTroisiemeArc(prochainRGB, incrementCouleur, angleDepartDegre);
+			}
+
+			private void dessinerPremierArc(int prochainRGB, int incrementCouleur, int angleDepartDegre) {
+				J.appel(this);
+
+				pinceau.setStroke(Color.rgb(220 + incrementCouleur, 220 + incrementCouleur, prochainRGB));
+				pinceau.setLineWidth(0.02*getWidth());
+				dessinerArc(0.8*TAILLE_PAR_DEFAUT_POURCENTAGE, angleDepartDegre);
+			}
+
+			private void dessinerDeuxiemeArc(int prochainRGB, int incrementCouleur, int angleDepartDegre) {
+				J.appel(this);
+
+				pinceau.setStroke(Color.rgb(220 + incrementCouleur, prochainRGB, 220 + incrementCouleur));
+				pinceau.setLineWidth(0.015*getWidth());
+				dessinerArc(0.6*TAILLE_PAR_DEFAUT_POURCENTAGE, (angleDepartDegre+90)%360);
+			}
+
+			private void dessinerTroisiemeArc(int prochainRGB, int incrementCouleur, int angleDepartDegre) {
+				J.appel(this);
+
+				pinceau.setStroke(Color.rgb(prochainRGB, 220 + incrementCouleur, 220 + incrementCouleur));
+				pinceau.setLineWidth(0.010*getWidth());
+				dessinerArc(0.4*TAILLE_PAR_DEFAUT_POURCENTAGE, (angleDepartDegre+180)%360);
+			}
+
+
 		};
 	}
 
@@ -100,8 +135,9 @@ public class CaseAjustable extends CanvasAjustable {
 				J.appel(this);
 				
 				animation.stop();
-				pinceau.setFill(Color.WHITE);
+
 				viderDessin();
+				initialiserPinceau();
 				dessinerCase(TAILLE_PAR_DEFAUT_POURCENTAGE);
 			}
 		});
@@ -111,8 +147,8 @@ public class CaseAjustable extends CanvasAjustable {
 	protected void reagirNouvelleLargeur(double ancienneLargeur, double nouvelleLargeur) {
 		J.appel(this);
 		
-		pinceau.setFill(Color.WHITE);
 		viderDessin();
+		initialiserPinceau();
 		dessinerCase(TAILLE_PAR_DEFAUT_POURCENTAGE);
 	}
 
@@ -120,9 +156,15 @@ public class CaseAjustable extends CanvasAjustable {
 	protected void reagirNouvelleHauteur(double ancienneHauteur, double nouvelleHauteur) {
 		J.appel(this);
 
-		pinceau.setFill(Color.WHITE);
 		viderDessin();
+		initialiserPinceau();
 		dessinerCase(TAILLE_PAR_DEFAUT_POURCENTAGE);
+	}
+
+	private void initialiserPinceau() {
+		pinceau.setFill(Color.WHITE);
+		pinceau.setStroke(Color.BLACK);
+		pinceau.setLineWidth(0.01*getWidth());
 	}
 	
 	private void viderDessin() {
@@ -132,42 +174,70 @@ public class CaseAjustable extends CanvasAjustable {
 		double hauteurDessin = getHeight();
 		
 		pinceau.clearRect(0, 0, largeurDessin, hauteurDessin);
-		
 	}
 
-	private void dessinerCase(double taille) {
+	private class Case {
+		public double caseHautGaucheX;
+		public double caseHautGaucheY;
+		public double tailleCase;
+	}
+
+	private void dessinerCase(double taillePourcentage) {
 		J.appel(this);
+		
+		Case laCase = calculerCase(taillePourcentage);
+		
+		pinceau.fillArc(laCase.caseHautGaucheX, 
+						laCase.caseHautGaucheY, 
+						laCase.tailleCase, 
+						laCase.tailleCase, 
+						0, 
+						360, 
+						ArcType.ROUND);
+
+		pinceau.strokeArc(laCase.caseHautGaucheX, 
+						  laCase.caseHautGaucheY, 
+						  laCase.tailleCase, 
+						  laCase.tailleCase, 
+						  0, 
+						  360, 
+						  ArcType.OPEN);
+	}
+
+
+	private void dessinerArc(double taillePourcentage, int angleDepartDegre) {
+		J.appel(this);
+		
+		Case laCase = calculerCase(taillePourcentage);
+		pinceau.strokeArc(laCase.caseHautGaucheX, 
+						  laCase.caseHautGaucheY, 
+						  laCase.tailleCase, 
+						  laCase.tailleCase, 
+						  angleDepartDegre, 
+						  90, 
+						  ArcType.OPEN);
+	}
+	
+	private Case calculerCase(double taillePourcentage) {
+		J.appel(this);
+		
+		Case laCase = new Case();
 
 		double largeurDessin = getWidth();
 		double hauteurDessin = getHeight();
 		
-		double tailleCase = largeurDessin * taille;
+		laCase.tailleCase = largeurDessin * taillePourcentage;
+
 		if(hauteurDessin < largeurDessin) {
-			tailleCase = hauteurDessin * taille;
+			laCase.tailleCase = hauteurDessin * taillePourcentage;
 		}
 		
-		double caseHautGaucheX = (largeurDessin - tailleCase) / 2;
-		double caseHautGaucheY = (hauteurDessin - tailleCase) / 2;
+		laCase.caseHautGaucheX = (largeurDessin - laCase.tailleCase) / 2;
+		laCase.caseHautGaucheY = (hauteurDessin - laCase.tailleCase) / 2;
 		
-		pinceau.fillArc(caseHautGaucheX, caseHautGaucheY, tailleCase, tailleCase, 0, 360, ArcType.ROUND);
-		
+		return laCase;
 	}
 
-	private void dessinerAnimation(double taille, int angleDepartDegre) {
-		J.appel(this);
-		
-		double largeurDessin = getWidth();
-		double hauteurDessin = getHeight();
-		
-		double tailleCase = largeurDessin * taille;
-		if(hauteurDessin < largeurDessin) {
-			tailleCase = hauteurDessin * taille;
-		}
-		
-		double caseHautGaucheX = (largeurDessin - tailleCase) / 2;
-		double caseHautGaucheY = (hauteurDessin - tailleCase) / 2;
-		
-		pinceau.strokeArc(caseHautGaucheX, caseHautGaucheY, tailleCase, tailleCase, angleDepartDegre, 90, ArcType.OPEN);
-		
-	}
+	
+	
 }
