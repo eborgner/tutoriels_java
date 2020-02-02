08ecea4a4dc3d78da0826ff6047e2a880a3c55c7
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-02-01 23:11:06 -0500

    en cours) animation

--- quatredesuite_javafx/src/main/java/quatredesuite_javafx/vues/composants/CaseAjustable.java
+++ quatredesuite_javafx/src/main/java/quatredesuite_javafx/vues/composants/CaseAjustable.java
@@ -6,12 +6,50 @@ import commun_javafx.vues.composants.CanvasAjustable;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 
+import javafx.animation.AnimationTimer;
+
 public class CaseAjustable extends CanvasAjustable {
+	
+	AnimationTimer animation;
+
+	public CaseAjustable() {
+		super();
+		
+		creerAnimation();
+		
+		animation.start();
+	}
+	
+	private void creerAnimation() {
+		J.appel(this);
+		
+		long depart = System.nanoTime();
+		
+		double tempsCycleSecondes = 1;
+		
+		animation = new AnimationTimer() {
+			@Override
+			public void handle(long maintenant) {
+				J.appel(this);
+				
+				double secondesEcoulees = (maintenant - depart) / 1E9;
+				
+				double instantDansCycle = (secondesEcoulees % tempsCycleSecondes) / tempsCycleSecondes;
+				
+				int rouge = (int) (255.0 * instantDansCycle);
+				
+				pinceau.setFill(Color.rgb(rouge, 255, 255));
+				dessinerCase();
+				
+			}
+		};
+	}
 
 	@Override
 	protected void reagirNouvelleLargeur(double ancienneLargeur, double nouvelleLargeur) {
 		J.appel(this);
 		
+		pinceau.setFill(Color.WHITE);
 		dessinerCase();
 	}
 
@@ -19,12 +57,12 @@ public class CaseAjustable extends CanvasAjustable {
 	protected void reagirNouvelleHauteur(double ancienneHauteur, double nouvelleHauteur) {
 		J.appel(this);
 
+		pinceau.setFill(Color.WHITE);
 		dessinerCase();
 	}
 
 	private void dessinerCase() {
 		J.appel(this);
-		
 		
 		double largeurDessin = getWidth();
 		double hauteurDessin = getHeight();
@@ -39,7 +77,6 @@ public class CaseAjustable extends CanvasAjustable {
 		double caseHautGaucheX = (largeurDessin - tailleCase) / 2;
 		double caseHautGaucheY = (hauteurDessin - tailleCase) / 2;
 		
-		pinceau.setFill(Color.WHITE);
 		pinceau.fillArc(caseHautGaucheX, caseHautGaucheY, tailleCase, tailleCase, 0, 360, ArcType.ROUND);
 		
 	}
