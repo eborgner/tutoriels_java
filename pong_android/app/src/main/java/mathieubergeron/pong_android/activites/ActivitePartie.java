236ade3a64849a669dd97ab3d0c09fac755cff4b
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-02-03 13:04:51 -0500

    début) vue

---
+++ pong_android/app/src/main/java/mathieubergeron/pong_android/activites/ActivitePartie.java
@@ -0,0 +1,16 @@
+package mathieubergeron.pong_android.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mathieubergeron.pong_android.R;
+
+public class ActivitePartie extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.page_partie);
+	}
+}
