8b1becf24ec3f178e699ed1285d674dc3cbcefea
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-02-03 13:27:54 -0500

    config de android_commun + ajout de CanvasAjustable

---
+++ commun_android/src/main/java/commun_android/vues/composants/CanvasAjustable.java
@@ -0,0 +1,26 @@
+package commun_android.vues.composants;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CanvasAjustable extends View {
+
+	public CanvasAjustable(Context context) {
+		super(context);
+	}
+
+	public CanvasAjustable(Context context, @Nullable AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public CanvasAjustable(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	public CanvasAjustable(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+	}
+}
