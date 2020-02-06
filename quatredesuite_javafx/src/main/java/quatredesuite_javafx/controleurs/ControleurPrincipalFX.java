746d881264ef432f5b0c213958abcbf366d1afd8
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-02-06 07:20:26 -0500

    ouvrir partie

--- quatredesuite_javafx/src/main/java/quatredesuite_javafx/controleurs/ControleurPrincipalFX.java
+++ quatredesuite_javafx/src/main/java/quatredesuite_javafx/controleurs/ControleurPrincipalFX.java
@@ -2,17 +2,21 @@ package quatredesuite_javafx.controleurs;
 
 import commun.debogage.J;
 import commun.systeme.Systeme;
+import commun_client.mvc.controleurs.FabriqueControleur;
 import commun_client.mvc.controleurs.RecepteurCommandeMVC;
 import commun_javafx.ChargeurDeVue;
 import commun_javafx.DialogueModal;
 import javafx.scene.Scene;
+import quatredesuite.modeles.partie.PartieLocale;
 import quatredesuite_client.commandes.nouvelle_partie.NouvellePartie;
 import quatredesuite_client.commandes.nouvelle_partie.NouvellePartieRecue;
 import quatredesuite_client.commandes.ouvrir_parametres.OuvrirParametres;
 import quatredesuite_client.commandes.ouvrir_parametres.OuvrirParametresRecue;
 import quatredesuite_client.commandes.quitter.Quitter;
 import quatredesuite_client.commandes.quitter.QuitterRecue;
+import quatredesuite_client.controleurs.ControleurPartieLocale;
 import quatredesuite_client.controleurs.ControleurPrincipal;
+import quatredesuite_javafx.afficheurs.AfficheurPartieLocaleFX;
 import quatredesuite_javafx.vues.VuePartieLocaleFX;
 import quatredesuite_javafx.vues.VuePrincipaleFX;
 import static quatredesuite_javafx.Constantes.*;
@@ -55,6 +59,12 @@ public class ControleurPrincipalFX extends ControleurPrincipal<VuePrincipaleFX> 
 		J.appel(this);
 		
 		VuePartieLocaleFX vuePartieLocale = vue.creerVuePartieLocale();
+		
+		PartieLocale partie = new PartieLocale();
+		
+		AfficheurPartieLocaleFX afficheur = new AfficheurPartieLocaleFX();
+		
+		FabriqueControleur.creerControleur(ControleurPartieLocaleFX.class, partie, vuePartieLocale, afficheur);
 		
 	}
 	
