a761c1bb92b71cf207e3c49c6c4c357183f270d0
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-02-06 12:02:05 -0500

    Vider Grille

---
+++ quatredesuite_client/src/main/java/quatredesuite_client/commandes/vider_grille/ViderGrillePourEnvoi.java
@@ -0,0 +1,7 @@
+package quatredesuite_client.commandes.vider_grille;
+
+import commun_client.commandes.CommandePourEnvoi;
+
+public interface ViderGrillePourEnvoi extends CommandePourEnvoi {
+
+}
---
+++ quatredesuite_client/src/main/java/quatredesuite_client/commandes/vider_grille/ViderGrilleRecue.java
@@ -0,0 +1,7 @@
+package quatredesuite_client.commandes.vider_grille;
+
+import commun_client.commandes.CommandeRecue;
+
+public interface ViderGrilleRecue extends CommandeRecue {
+
+}
