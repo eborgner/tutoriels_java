d0d9fea6be0b33ddac92049ed37a160e3f406208
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-02-09 04:28:23 -0500

    Convertir coordonnées dans Afficheur

--- quatredesuite_client/src/main/java/quatredesuite_client/afficheurs/AfficheurPartieLocale.java
+++ quatredesuite_client/src/main/java/quatredesuite_client/afficheurs/AfficheurPartieLocale.java
@@ -13,62 +13,82 @@ import quatredesuite_client.vues.VuePartieLocale;
 
 public abstract class AfficheurPartieLocale<V extends VuePartieLocale> 
 
-							extends Afficheur<PartieLocaleLectureSeule, 
-											  V> {
+                            extends Afficheur<PartieLocaleLectureSeule, 
+                                              V> {
 
-	@Override
-	public void initialiserAffichage(PartieLocaleLectureSeule partieLectureSeule, VuePartieLocale vue) {
-		J.appel(this);
-		
-		int largeur = partieLectureSeule.getLargeur();
-		int hauteur = partieLectureSeule.getHauteur();
-		
-		vue.creerGrille(largeur, hauteur);
-	}
+    @Override
+    public void initialiserAffichage(PartieLocaleLectureSeule partieLectureSeule, VuePartieLocale vue) {
+        J.appel(this);
+        
+        int largeur = partieLectureSeule.getLargeur();
+        int hauteur = partieLectureSeule.getHauteur();
+        
+        vue.creerGrille(largeur, hauteur);
+    }
 
-	@Override
-	public void rafraichirAffichage(PartieLocaleLectureSeule partieLectureSeule, VuePartieLocale vue) {
-		J.appel(this);
+    @Override
+    public void rafraichirAffichage(PartieLocaleLectureSeule partieLectureSeule, VuePartieLocale vue) {
+        J.appel(this);
 
-		GrilleLectureSeule grille = partieLectureSeule.getGrille();
-		
-		rafraichirGrille(grille, vue);
-		
-		JetonLectureSeule dernierJeton = partieLectureSeule.getDernierJetonAjoute();
-		
-		if(dernierJeton != null) {
-			vue.animerEntreeJeton(dernierJeton.getIndiceColonne(), dernierJeton.getIndiceRangee());
-		}
-	}
+        GrilleLectureSeule grille = partieLectureSeule.getGrille();
+        int hauteurGrille = partieLectureSeule.getHauteur();
+        
+        rafraichirGrille(hauteurGrille, grille, vue);
+        
+        JetonLectureSeule dernierJeton = partieLectureSeule.getDernierJetonAjoute();
+        
+        if(dernierJeton != null) {
+            int indiceRangeeGraphiqueDernierJeton = indiceRangeeEnCoordonneesGraphiques(dernierJeton.getIndiceRangee());
+            vue.animerEntreeJeton(dernierJeton.getIndiceColonne(), indiceRangeeCoordonneesGraphiques);
+        }
+    }
 
-	private void rafraichirGrille(GrilleLectureSeule grille, VuePartieLocale vue) {
-		J.appel(this);
+    private void rafraichirGrille(int hauteurGrille, GrilleLectureSeule grille, VuePartieLocale vue) {
+        J.appel(this);
 
-		List<ColonneLectureSeule> colonnes = grille.getColonnes();
-		
-		for(int indiceColonne = 0; indiceColonne < colonnes.size(); indiceColonne++) {
-		
-			ColonneLectureSeule colonne = colonnes.get(indiceColonne);
-			List<JetonLectureSeule> jetons = colonne.getJetons();
-			
-			rafraichirColonne(indiceColonne, jetons, vue);
+        List<ColonneLectureSeule> colonnes = grille.getColonnes();
+        
+        for(int indiceColonne = 0; indiceColonne < colonnes.size(); indiceColonne++) {
+        
+            ColonneLectureSeule colonne = colonnes.get(indiceColonne);
+            List<JetonLectureSeule> jetons = colonne.getJetons();
+            
+            rafraichirColonne(hauteurGrille, indiceColonne, jetons, vue);
 
-		}
-	}
+        }
+    }
 
-	private void rafraichirColonne(int indiceColonne, 
-								   List<JetonLectureSeule> jetons, 
-								   VuePartieLocale vue) {
-		J.appel(this);
-		
-		
-		for(int indiceRangee = 0; indiceRangee < jetons.size(); indiceRangee++) {
-			
-			JetonLectureSeule jeton = jetons.get(indiceRangee);
-			Couleur couleur = jeton.getCouleur();
-			
-			vue.afficherJeton(indiceColonne, indiceRangee, couleur);
-		}
-	}
+    private void rafraichirColonne(int hauteurGrille, 
+                                   int indiceColonne, 
+                                   List<JetonLectureSeule> jetons, 
+                                   VuePartieLocale vue) {
+        J.appel(this);
+        
+        
+        for(int indiceRangee = 0; indiceRangee < jetons.size(); indiceRangee++) {
+            
+            JetonLectureSeule jeton = jetons.get(indiceRangee);
+            Couleur couleur = jeton.getCouleur();
+            
+            afficherJeton(hauteurGrille, indiceColonne, indiceRangee, couleur, vue);
+        }
+    }
+    
+    private void afficherJeton(int hauteurGrille, 
+                               int indiceColonne,  
+                               int indiceRangee, 
+                               Couleur couleur,
+                               VuePartieLocale vue) {
+        J.appel(this);
+        
+        int indiceRangeeCoordonneesGraphiques = indiceRangeeEnCoordonneesGraphiques(hauteurGrille, indiceRangee);
 
+        vue.afficherJeton(indiceColonne, indiceRangeeCoordonneesGraphiques, couleur);
+    }
+    
+    private int indiceRangeeEnCoordonneesGraphiques(int hauteurGrille, int indiceRangee) {
+        J.appel(this);
+
+        return hauteurGrille - indiceRangee - 1;
+    }
 }
