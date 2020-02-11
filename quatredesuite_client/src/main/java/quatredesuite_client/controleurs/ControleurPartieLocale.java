cc82314ff3f71435c2f84bbe5dc7c2c747951195
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-02-10 20:48:12 -0500

    controleur

--- quatredesuite_client/src/main/java/quatredesuite_client/controleurs/ControleurPartieLocale.java
+++ quatredesuite_client/src/main/java/quatredesuite_client/controleurs/ControleurPartieLocale.java
@@ -2,24 +2,29 @@ package quatredesuite_client.controleurs;
 
 import commun.debogage.J;
 import commun_client.mvc.controleurs.ControleurModeleVue;
+import commun_client.mvc.controleurs.RecepteurCommandeMVC;
 import quatredesuite.modeles.partie.PartieLocale;
 import quatredesuite.modeles.partie.PartieLocaleLectureSeule;
 import quatredesuite_client.afficheurs.AfficheurPartieLocale;
 import quatredesuite_client.vues.VuePartieLocale;
 
-public class ControleurPartieLocale extends ControleurModeleVue<PartieLocaleLectureSeule, 
-															    PartieLocale, 
-															    VuePartieLocale, 
-															    AfficheurPartieLocale> {
+public abstract class ControleurPartieLocale<V extends VuePartieLocale,
+					       				     A extends AfficheurPartieLocale<V>> 
+
+					extends ControleurModeleVue<PartieLocaleLectureSeule, 
+											    PartieLocale, 
+											    V, 
+											    A> {
 	
 	@Override
-	public void demarrer() {
+	protected void demarrer() {
 		J.appel(this);
+
 	}
 
 	@Override
-	public void installerReceptionCommandes() {
+	protected void installerReceptionCommandes() {
 		J.appel(this);
+
 	} 
-	
 }
