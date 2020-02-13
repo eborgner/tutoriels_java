39fdae8fcc8a3a4395762c44fcb616113fd5cd09
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2020-02-13 05:53:58 -0500

    retour à List pour les entetes

--- quatredesuite_javafx/src/main/java/quatredesuite_javafx/vues/composants/ConteneurEntetes.java
+++ quatredesuite_javafx/src/main/java/quatredesuite_javafx/vues/composants/ConteneurEntetes.java
@@ -1,34 +1,42 @@
 package quatredesuite_javafx.vues.composants;
 
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 
 import commun.debogage.J;
 import javafx.beans.NamedArg;
+import javafx.scene.Node;
 import javafx.scene.layout.HBox;
 
 public class ConteneurEntetes extends HBox {
-	
-	private String texteBouton;
-	
-	public ConteneurEntetes(@NamedArg("texteBouton") String texteBouton) {
-		J.appel(this);
-		
-		this.texteBouton = texteBouton;
-	}
+    
+    private String texteBouton;
+    
+    public ConteneurEntetes(@NamedArg("texteBouton") String texteBouton) {
+        J.appel(this);
+        
+        this.texteBouton = texteBouton;
+    }
 
-	public void creerEntetes(int largeur) {
-		J.appel(this);
-		
-		for(int indiceColonne = 0; indiceColonne < largeur; indiceColonne++) {
-			
-			this.getChildren().add(new Entete(indiceColonne, texteBouton));
-		}
-	}
-	
-	
-	private Stream<Entete> entetes(){
-		J.appel(this);
+    public void creerEntetes(int largeur) {
+        J.appel(this);
+        
+        for(int indiceColonne = 0; indiceColonne < largeur; indiceColonne++) {
+            
+            this.getChildren().add(new Entete(indiceColonne, texteBouton));
+        }
+    }
 
-		return this.getChildren().stream().map(child -> (Entete) child);
-	}
+    private List<Entete> entetes(){
+        J.appel(this);
+        
+        List<Entete> entetes = new ArrayList<>();
+        
+        for(Node enfant : this.getChildren()) {
+            
+            entetes.add((Entete) enfant);
+        }
+
+        return entetes;
+    }
 }
